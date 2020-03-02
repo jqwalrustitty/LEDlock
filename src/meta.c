73c6a4de718d624ff57b32186d9e095474d5e9bd
jqwalrus <rodger.allen@protonmail.com>
2020-03-02 18:31:17 +1100

    added encoding field to header

--- src/meta.c
+++ src/meta.c
@@ -8,17 +8,13 @@
 #include <stdio.h>          // FILE, fopen(), fclose(), fread(), ...
 #include <stdlib.h>         // malloc(), exit()
 #include <string.h>         // strlen(), memcpy()
+#include <inttypes.h>
 
 #include "meta.h"
 #include "crc32.h"
 
-// -------------------------------------
-// TODO: sort out the MAGIX
-uint8_t magix[] = { 'C', 'Q', 'C', 'Q' };
-
-/* -------------------------------------------------------------------------- */
-/*
- * Converting bytes to/from metadata
+/* -----------------------------------------------------------------------------
+ * Metadata Manipulators
  */
 
 /* -------------------------------------
@@ -26,124 +22,111 @@ uint8_t magix[] = { 'C', 'Q', 'C', 'Q' };
  */
 int metaSize( struct metadata *md )
 {
-    return (sizeof(md->size) + sizeof(md->crc));
+    return (  sizeof(md->mask) + sizeof(md->delay)
+            + sizeof(md->size) + sizeof(md->crc));
 }
 
 /* -------------------------------------
- * convert metadata struct to string of bytes
+ * Convert metadata struct to string of bytes
  * returns number of bytes
  */
 int metaToBytes( uint8_t **buf, struct metadata *md )
 {
     int sz = metaSize(md);
+    int inc = 0;
     *buf = malloc(sz);
-    memcpy(*buf,                    (char *) &md->size, sizeof(md->size));
-    memcpy(*buf + sizeof(md->size), (char *) &md->crc, sizeof(md->crc));
+    memcpy(*buf + inc, (uint8_t *) &md->crc,   sizeof(md->crc));    inc += sizeof(md->crc);
+    memcpy(*buf + inc, (uint8_t *) &md->size,  sizeof(md->size));   inc += sizeof(md->size);
+    memcpy(*buf + inc, (uint8_t *) &md->delay, sizeof(md->delay));  inc += sizeof(md->delay);
+    memcpy(*buf + inc, (uint8_t *) &md->mask,  sizeof(md->mask));   inc += sizeof(md->mask);
     return sz;
 }
 
 /* -------------------------------------
- * convert string of bytes to a metadata struct
+ * Convert string of bytes to a metadata struct
  */
 struct metadata bytesToMeta( uint8_t *buf )
 {
     struct metadata md;
-    memcpy(&(md.size), buf,                     sizeof(md.size));
-    memcpy(&(md.crc), (buf + sizeof(md.size)),  sizeof(md.crc));
+    int inc = 0;
+    memcpy(&(md.crc),   buf + inc,  sizeof(md.crc));    inc += sizeof(md.crc);
+    memcpy(&(md.size),  buf + inc,  sizeof(md.size));   inc += sizeof(md.size);
+    memcpy(&(md.delay), buf + inc,  sizeof(md.delay));  inc += sizeof(md.delay);
+    memcpy(&(md.mask),  buf + inc,  sizeof(md.mask));   inc += sizeof(md.mask);
     return md;
 }
 
-/* -------------------------------------------------------------------------- */
-/*
- * Create metadata for files
+/* -------------------------------------
+ * Metadata to string
+ */
+char * showMetaData( metadata *md )
+{
+    static char str[128] = {0};
+    snprintf(str,128,"mask=%s delay=%d size=%u crc=0x%#08x",
+                  showLEDs(md->mask), md->delay, md->size, md->crc);
+    return str;
+}
+
+/* -------------------------------------
+ *  Check validity of metadata
+ *  - returns 0 if false
+ *  - crc can only be zero if filesize is zero
+ *  - reject if md mask doesn't match enc mask
+ */
+int metaSanity ( struct metadata *md, encoder *enc )
+{
+    if (md->crc==0 && md->size!=0)
+        return 0;
+    if (md->crc!=0 && md->size==0)
+        return 0;
+    if (md->mask != enc->mask)
+        return 0;
+    //if (md->size == 0) return 0;
+    return 1;
+}
+/* -----------------------------------------------------------------------------
+ * Construct metadata from file
  */
 
 /* -------------------------------------
- * Read a file and create metadata
- *  'filename'  : file
+ * Read from a file-handle and create metadata
+ *  'fh'        : file handle (can be stdin)
  *  'buf'       : ptr to buffer of file payload 
- *  returns     : metadat of file
+ *  returns     : metadata of file (size,crc)
  */
-struct metadata getFile( const char *filename, uint8_t **buf )
+struct metadata getFileH( FILE *fh, uint8_t **buf, encoder enc, int timer )
 {
     struct metadata md = {0};
     size_t sz = 0;
-    FILE *fh = fopen(filename,"rb");
-    if (fh==NULL) return md;
+    uint8_t inbuf[MAXFILESIZE] = {0};
+    size_t n = 0;
+
     fseek(fh, 0, SEEK_END);
     sz = ftell(fh);
     rewind(fh);
-    if (sz >= MAXFILESIZE) {
-        fprintf(stderr,"ERR: max file size exceeded: %d > %d\n",(int)sz,MAXFILESIZE);
-        return md;
+    if ((int)sz == -1) {
+        VERBOSE(1)fprintf(stderr,"using stdin\n");
+        for(sz=0;sz<MAXFILESIZE&&(!feof(stdin)); sz++) {
+            n = fread(&(inbuf[(int)sz]),1,1,stdin);
+            if(n==0) break;
+        }
+        *buf = malloc(sz);
+        memcpy(*buf,inbuf,sz);
+    } else {
+        if (sz > MAXFILESIZE) {
+            fprintf(stderr,"ERR: max file size exceeded: %d > %d\n",(int)sz,MAXFILESIZE);
+            return md;
+        }
+        *buf = malloc(sz);
+        fread(*buf, sz, 1, fh);
     }
-    *buf = malloc(sz);
-    fread(*buf, sz, 1, fh);
+    md.mask = enc.mask;
+    md.delay = timer;
     md.size = sz;
     md.crc = rc_crc32(0, *buf, sz);
-    fclose(fh);
     return md;
 }
 
-/* -------------------------------------------------------------------------- */
-#if DEBUG
-/* -------------------------------------
- * not sure yet what sanity means
- */
-int metaSanity( struct metadata *md )
-{
-    if (md->size == 0)
-        return 0;
-    return 1;
-}
-#endif
-
-/* -------------------------------------------------------------------------- */
-/*
- * Display some of the metadata
- */
-
-/* -------------------------------------
- *
- */
-#ifdef IMPLANT
-void showMetadata( metadata *md )
-{
-    fprintf(stdout,"size: %u\ncrc %#x\n", md->size, md->crc);
-    //fprintf(stdout,"size: %u\n", md->size);
-    //fprintf(stdout,"crc: %#x\n", md->crc);
-}
-#endif
-
-#if DEBUG
-/* -------------------------------------
- *
- */
-void showMetadataStr( char * str, int sz, metadata *md )
-{
-    snprintf(str,sz,"size:%u, crc:%#x",
-            md->size, md->crc);
-}
-
-/* -------------------------------------
- *
- */
-void showMetadataRaw( metadata *md )
-{
-    unsigned int i;
-    char filesz[sizeof(md->size)];
-    char crc32[sizeof(md->crc)];
-
-    memcpy(filesz, (char *) & md->size, sizeof(md->size));
-    memcpy(crc32,  (char *) & md->crc,  sizeof(md->crc));
-
-    for (i=0; i<sizeof(md->size); i++)
-        fprintf(stdout,"%c", filesz[i]);
-
-    for (i=0; i<sizeof(md->crc); i++)
-        fprintf(stdout,"%c", crc32[i]);
-}
-#endif
 
 // -----------------------------------------------------------------------------
 // vi: et ts=4 ai
--- src/meta.h
+++ src/meta.h
@@ -9,45 +9,35 @@
 #define __META_H_
 
 #include "global.h"
-#include <inttypes.h>   // uint32_t
+#include "encoder.h"
+#include <inttypes.h>
 
 /* -------------------------------------
  * Metadata definitions
  */
-
 typedef struct metadata {
+    uint8_t mask;
+    uint8_t delay;
     uint16_t size;
     uint32_t crc;
 } metadata;
 
 // Implicit in metadata.size - 255 or 65535
-#define MAXFILESIZE ((0x100 << (8*(sizeof(uint16_t)-1))) - 1)
-
-// TODO: sort out the MAGIX
-uint8_t magix[4];
+#define MAXFILESIZE (0xffff)
 
 /* -------------------------------------
- * Utilities
+ * Construct metadata from file
  */
+struct metadata getFileH( FILE *fh, uint8_t **buf, encoder enc, int timer );
 
-#ifdef IMPLANT
-struct metadata getFile( const char *filename, uint8_t **buf );
-void test_getFile( char *filename );
-int metaToBytes( uint8_t **buf, struct metadata *md );
-void showMetadata( metadata *md );
-#endif
-
-#ifdef LISTENER
-int metaSize( struct metadata *md );                    // kbdinfil
-struct metadata bytesToMeta( uint8_t *buf );            // kbdinfil
-#endif
-
-#if DEBUG
-//void showMetadata( metadata *md );                      // kbdexfil,kbdinfil
-void showMetadataStr( char * str, int sz, metadata *md );   // kbdinfil
-void showMetadataRaw( metadata *md );
-int metaSanity( struct metadata *md );
-#endif
+/* -------------------------------------
+ * Metadata Manipulators
+ */
+int                 metaToBytes    ( uint8_t **buf, struct metadata *md );
+int                 metaSize       ( struct metadata *md );
+struct metadata     bytesToMeta    ( uint8_t *buf );
+char *              showMetaData   ( metadata *md );
+int                 metaSanity     ( struct metadata *md, encoder *enc );
 
 #endif
 // -----------------------------------------------------------------------------
